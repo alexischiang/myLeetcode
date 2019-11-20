342a514463be53d5c885c5d1fd5a6aa7a95f959a
alexischiang <1091285927@qq.com>
2019-11-20 22:18:24 +0800

    1.twoSum

--- 1.js
+++ 1.js
@@ -6,13 +6,13 @@
 
 // 利用数组做映射
 var twoSum = function(nums, target) {
-	let temp = [];
-	for (let i = 0 ; i<nums.length ; i++){
-			a = target - nums[i]
-			if(temp[a] != undefined){
-					return [temp[a],i]
-			} else {
-					temp[nums[i]] = i
-			}
-	}
+  let temp = [];
+  for (let i = 0; i < nums.length; i++) {
+    a = target - nums[i];
+    if (temp[a] != undefined) {
+      return [temp[a], i];
+    } else {
+      temp[nums[i]] = i;
+    }
+  }
 };
